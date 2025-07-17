7b4a77e3381752705f2cf979644d3d164b014e44
付赞鸿 <fuzanhong@gmail.com>
2025-07-17 15:28:22 +0800

    111

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,17 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet, View, Text } from 'react-native';
+import { Platform, StyleSheet, View, Text, Button, useColorScheme, Appearance } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { Link } from 'expo-router';
+import { useAppStore } from '@/stores/appStore';
 
 export default function HomeScreen() {
+
+  const appStore = useAppStore()
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -53,14 +57,28 @@ export default function HomeScreen() {
         </ThemedText>
       </ThemedView>
       <View>
-        <Link href={'/demo'}>setting lang</Link>
-        <Text className='text-red'>123</Text>
+        <Text className='dark:text-white'>{appStore.language}</Text>
       </View>
-      <View className="flex-1 items-center justify-center bg-white">
-        <Text className="text-xl font-bold text-blue-500">
-          Welcome to Nativewind!
-        </Text>
-      </View>
+      <Button title='set'
+        onPress={() => {
+          appStore.setLanguage('en-US')
+        }}
+      />
+      <Button title='null'
+        onPress={() => {
+          Appearance.setColorScheme(null)
+        }}
+      />
+      <Button title='light'
+        onPress={() => {
+          Appearance.setColorScheme('light')
+        }}
+      />
+      <Button title='dark'
+        onPress={() => {
+          Appearance.setColorScheme('dark')
+        }}
+      />
     </ParallaxScrollView>
   );
 }
--- stores/appStore.ts
+++ stores/appStore.ts
@@ -3,10 +3,8 @@ import { persist, createJSONStorage } from 'zustand/middleware';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface AppState {
-  theme: 'light' | 'dark' | 'auto';
   language: 'zh-CN' | 'en-US';
   isFirstLaunch: boolean;
-  setTheme: (theme: 'light' | 'dark' | 'auto') => void;
   setLanguage: (language: 'zh-CN' | 'en-US') => void;
   setFirstLaunch: (isFirst: boolean) => void;
 }
@@ -14,10 +12,8 @@ interface AppState {
 export const useAppStore = create<AppState>()(
   persist(
     (set) => ({
-      theme: 'auto',
       language: 'zh-CN',
       isFirstLaunch: true,
-      setTheme: (theme) => set({ theme }),
       setLanguage: (language) => set({ language }),
       setFirstLaunch: (isFirst) => set({ isFirstLaunch: isFirst }),
     }),
